6575645f1ee0da59c6245f4577714ae09d5ab872
Kyaw Thiha <kth4dev@gmail.com>
2024-05-17 12:56:17 +0700

    chore(res):add icons

---
+++ lib/core/images/icons/folders/arrow_chevron.dart
@@ -0,0 +1,8 @@
+import 'package:dlog/core/utils/constants.dart';
+
+class ArrowChevron {
+  final arrowDown = "$iconsPath/Arrows-chevron/Arrow-Down.svg";
+  final chevronLeft = "$iconsPath/Arrows-chevron/Chevron-Left.svg";
+  final chevronRight = "$iconsPath/Arrows-chevron/chevron-right.svg";
+  final chevronUp = "$iconsPath/Arrows-chevron/chevron-up.svg";
+}
---
+++ lib/core/images/icons/icons.dart
@@ -0,0 +1,6 @@
+import 'folders/arrow_chevron.dart';
+
+//todo : more declaration & don't change file name , we will replace icon folder from figma
+class DLogIcons {
+  static ArrowChevron chevron = ArrowChevron();
+}
