3eb2712ba4b0a07f89ccfb1c937e64832f309073
Ye Lin Htet <mgyelin05@gmail.com>
2024-05-28 16:34:40 +0630

    feat(successful-bottom-sheet) : develop successful bottom sheet

---
+++ lib/features/auth/presentation/screens/successful_bottom_sheet/show_successful_bottom_sheet.dart
@@ -0,0 +1,66 @@
+import 'package:dlog/core/extensions/context_extension.dart';
+import 'package:dlog/core/extensions/num_extension.dart';
+import 'package:dlog/core/images/graphic/graphic.dart';
+import 'package:dlog/core/ui/image/svg_image.dart';
+import 'package:dlog/core/ui/text/dlog_text.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+
+class SuccessfulBottomSheet extends StatefulWidget {
+  final String text;
+  final String route;
+  const SuccessfulBottomSheet({super.key, required this.text,required this.route});
+
+  @override
+  State<SuccessfulBottomSheet> createState() => _SuccessfulBottomSheetState();
+}
+
+class _SuccessfulBottomSheetState extends State<SuccessfulBottomSheet> {
+
+  @override
+  void initState() {
+   _initRoute();
+    super.initState();
+  }
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      height: 321,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const DLogSvgImage(
+            path: DLogGraphic.confirmed,
+            width: 178,
+            height: 210,
+          ),
+          22.spacingHeight,
+          DLogText(widget.text, style: context.getTextTheme.tertiaryHeaderBold)
+        ],
+      ),
+    );
+  }
+  _initRoute() async {
+    await Future.delayed(
+      const Duration(seconds: 1),
+          () => context.go(widget.route),
+    );
+  }
+}
+
+ void showSuccessfulBottomSheet(
+    {required BuildContext context, required String text,required String route}) {
+  showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topLeft: Radius.circular(20),
+          topRight: Radius.circular(20),
+        )
+      ),
+      backgroundColor: context.getColorScheme.whiteColor,
+      builder: (context) => SuccessfulBottomSheet(text: text,route: route,));
+}
