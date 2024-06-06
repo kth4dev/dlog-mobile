fae8f8859a81aec74f7ff8d469bf9ebcb01c42f5
Ye Lin Htet <mgyelin05@gmail.com>
2024-06-06 23:15:57 +0630

    refactor(home) : repair delivered order

--- lib/features/home/presentation/screens/delivered_order/detail/views/route/items/item.dart
+++ lib/features/home/presentation/screens/delivered_order/detail/views/route/items/item.dart
@@ -1,4 +1,5 @@
 import 'package:dlog/core/extensions/context_extension.dart';
+import 'package:dlog/core/images/icons.dart';
 import 'package:dlog/core/ui/image/svg_image.dart';
 import 'package:dlog/core/ui/text/dlog_text.dart';
 import 'package:flutter/material.dart';
@@ -32,7 +33,10 @@ class RouteDetailItem extends StatelessWidget {
           color: context.getColorScheme.grey.lightActive,
         ),
         const LocationItem(label: "Delivery From", location: "Yangon"),
-        const DLogSvgImage(path: "assets/images/icons/Arrows-chevron/Arrow 4.svg",width: 20,height: 20,),
+        const Padding(
+          padding: EdgeInsets.symmetric(horizontal: 6.5),
+          child: DLogSvgImage(path: DLogIcons.downArrowRoute,width: 20,height: 20,),
+        ),
         const LocationItem(label: "Delivery To", location: "Mandalay"),
 
       ],
